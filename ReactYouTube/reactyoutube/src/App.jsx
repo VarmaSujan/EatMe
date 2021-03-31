b2494c9f3493ceb8832b90f6d0e91222a5ea2ee4
VArmaSujan <sujanjcj@gmail.com>
2021-03-31 09:36:28 +0200

    Minor update

---
+++ ReactYouTube/reactyoutube/src/App.jsx
@@ -0,0 +1,28 @@
+import YouTube from "react-youtube";
+import { useState } from "react";
+
+export default function App() {
+  const [videoId, setVideoId] = useState("1_Aq5rQdPso");
+
+  const options = {
+    height: "390",
+    width: "640",
+    playerVars: { autoplay: 1 },
+  };
+
+  return (
+    <div className="App">
+      <YouTube opts={options} videoId={videoId} />
+
+      <button onClick={() => setVideoId("1_Aq5rQdPso")}>
+        Golden Eye
+      </button>
+
+      <button onClick={() => setVideoId("rwDUWnOVf0c")}>
+        Tomorrow Never Dies
+      </button>
+    </div>
+  );
+}
+
+
