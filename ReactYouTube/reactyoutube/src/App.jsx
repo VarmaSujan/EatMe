e52a92f619d6e9a7990e6e24a462d65fde253276
VArmaSujan <sujanjcj@gmail.com>
2021-04-02 21:42:43 +0200

    Initial commit

--- ReactYouTube/reactyoutube/src/App.jsx
+++ ReactYouTube/reactyoutube/src/App.jsx
@@ -1,6 +1,5 @@
 import YouTube from "react-youtube";
 import { useState } from "react";
-import {Route, BrowserRouter as router}
 
 export default function App() {
   const [videoId, setVideoId] = useState("1_Aq5rQdPso");
@@ -15,9 +14,7 @@ export default function App() {
     <div className="App">
       <YouTube opts={options} videoId={videoId} />
 
-      <button onClick={() => setVideoId("1_Aq5rQdPso")}>
-        Golden Eye
-      </button>
+      <button onClick={() => setVideoId("1_Aq5rQdPso")}>Golden Eye</button>
 
       <button onClick={() => setVideoId("rwDUWnOVf0c")}>
         Tomorrow Never Dies
@@ -25,5 +22,3 @@ export default function App() {
     </div>
   );
 }
-
-
